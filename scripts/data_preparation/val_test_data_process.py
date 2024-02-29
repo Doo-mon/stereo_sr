f4ec97e70979bafaff3238c2e44e5425aa7017c3
Doo-mon <zhanzhh6@mail2.sysu.edu.cn>
2024-02-29 14:32:34 +0800

    fix some bugs in data process

--- scripts/data_preparation/val_test_data_process.py
+++ scripts/data_preparation/val_test_data_process.py
@@ -8,7 +8,7 @@ def parser_setting():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", default="../../datasets", type=str)
     parser.add_argument("--dataset_name", default="Flickr1024", type=str)
-    parser.add_argument("--data_type", default="Validation",choices=["Validation","Test"], type=str)
+    parser.add_argument("--data_type", default="Validation",choices=["Validation", "Test"], type=str)
     parser.add_argument("--scale", default=2, type=int) # 设置放缩尺寸
 
     return parser.parse_args()
@@ -22,7 +22,7 @@ def modcrop(image, scale):
     return image
 
 # 通过Bicubic获得低分辨率图像
-def process_images(img_path_0, scale):
+def process_images_1(img_path_0, scale):
     img_0 = Image.open(img_path_0)
     img_hr_0 = modcrop(img_0, scale) 
     img_lr_0 = img_hr_0.resize((img_hr_0.size[0] // scale, img_hr_0.size[1] // scale), Image.BICUBIC)
@@ -39,8 +39,7 @@ def main():
     # 保存文件夹路径
     if args.data_type == 'Test':
         output_dir = os.path.join(args.data_dir, 'test_data')
-        output_dir = os.path.join(output_dir, args.dataset_name) # 对于测试集 需要多保存一个数据集名字的文件夹
-        
+        output_dir = os.path.join(output_dir, args.dataset_name) # 对于测试集 需要多保存一个数据集名字的文件夹(考虑到会用多个不同的数据集进行测试)
     else:
         output_dir = os.path.join(args.data_dir, 'val_data')
 
@@ -59,25 +58,24 @@ def main():
     hr_dir = os.path.join(output_dir, 'hr')
     lr_dir = os.path.join(output_dir, f'lr_x{scale}')
 
-
-    for i in range(0,len(imgs_list),2):
-        img_hr_0, img_lr_0 = process_images(imgs_list[i], scale = scale)
-        img_hr_1, img_lr_1 = process_images(imgs_list[i+1], scale = scale)
+    # 成对处理图像
+    for i in range(0, len(imgs_list), 2):
+        img_hr_0, img_lr_0 = process_images_1(imgs_list[i], scale = scale)
+        img_hr_1, img_lr_1 = process_images_1(imgs_list[i + 1], scale = scale)
 
         img_name = os.path.basename(imgs_list[i])
-        img_name = os.path.splitext(img_name)[0]
-        img_name = img_name.split('_')[0]
+        img_name = os.path.splitext(img_name)[0] # 0001_L
+        img_name = img_name.split('_')[0] # 0001
 
         hr_img_dir = os.path.join(hr_dir, f'{img_name}')
         lr_img_dir = os.path.join(lr_dir, f'{img_name}')
         os.makedirs(hr_img_dir, exist_ok=True)
         os.makedirs(lr_img_dir, exist_ok=True)
-
-        
-        Image.fromarray(np.uint8(img_hr_0)).save(os.path.join(hr_img_dir, 'hr_0.png'))
-        Image.fromarray(np.uint8(img_hr_1)).save(os.path.join(hr_img_dir, 'hr_1.png'))
-        Image.fromarray(np.uint8(img_lr_0)).save(os.path.join(lr_img_dir, 'lr_0.png'))
-        Image.fromarray(np.uint8(img_lr_1)).save(os.path.join(lr_img_dir, 'lr_1.png'))
+     
+        Image.fromarray(np.uint8(img_hr_0)).save(os.path.join(hr_img_dir, 'hr0.png'))
+        Image.fromarray(np.uint8(img_hr_1)).save(os.path.join(hr_img_dir, 'hr1.png'))
+        Image.fromarray(np.uint8(img_lr_0)).save(os.path.join(lr_img_dir, 'lr0.png'))
+        Image.fromarray(np.uint8(img_lr_1)).save(os.path.join(lr_img_dir, 'lr1.png'))
         print(f'{i} {args.data_type} samples have been generated...')
 
 
