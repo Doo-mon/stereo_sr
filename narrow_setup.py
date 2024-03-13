7e97d562a44ebe8d0f0890dbedc55df4756da017
Doo-mon <zhanzhh6@mail2.sysu.edu.cn>
2024-03-13 22:24:38 +0800

    fix some bugs

--- narrow_setup.py
+++ narrow_setup.py
@@ -3,23 +3,24 @@ import sys
 import time
 import argparse
 
+# 输入命令
+# python narrow_setup.py --total_gpu 6 --need_gpu 2 --interval 2
 
 # train.py
-cmd = "python -m torch.distributed.launch --nproc_per_node=1 --master_port=29500  \
-    ~/stereo_sr/train.py \
-    -opt ./options/base_model_train_4x_T.yml"
+cmd = "python -m torch.distributed.launch --nproc_per_node=2 --master_port=29500  \
+       ~/stereo_sr/train.py \
+       -opt ./options/base_model_train_4x_T.yml"
 
 # test.py
 cmd1 = "python -m torch.distributed.launch --nproc_per_node=1 --master_port=29500  \
         ~/stereo_sr/test.py \
         -opt ./options/base_model_test_4x_T.yml"
 
-# CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.launch --nproc_per_node=1 --master_port=29500  ~/stereo_sr/test.py -opt ./options/base_model_test_4x_T.yml
 
 def parse_setting():
     parser = argparse.ArgumentParser(description='narrow setup')
 
-    parser.add_argument("--total_gpu", default=4, type=int, help="number of gpu")
+    parser.add_argument("--total_gpu", default=6, type=int, help="number of gpu")
     parser.add_argument("--need_gpu", default=2, type=int, help="number of gpu")
     parser.add_argument("--interval", default=2, type=int, help="interval time for checking gpu status")
 
@@ -79,17 +80,15 @@ def narrow_setup_multi_gpu(command, interval = 2, total_gpu = 8, need_gpu = 8):
     selected_gpu = []
     mark_list =  [0] * total_gpu
     count = 0
-    while count >= need_gpu:
+    while count < need_gpu:
         i = i % 5
         for n in range(total_gpu): # 循环获取所有gpu的信息再进行判断
             gpu_power, gpu_memory = gpu_info(gpu_index = n)
-            if gpu_memory < 1000 and gpu_power < 20:
-                if mark_list[n] == 0:
+            if gpu_memory < 1000 and gpu_power < 20 and mark_list[n] == 0:
                     selected_gpu.append(n)
                     mark_list[n] = 1
                     count += 1    
-            else:
-                if mark_list[n] == 1:  # 不符合 => 重新检查存储表 这个时候一般代表卡又被占了
+            elif mark_list[n] == 1: # 不符合 => 重新检查存储表 这个时候一般代表卡又被占了
                     selected_gpu.remove(n)
                     mark_list[n] = 0
                     count -= 1
