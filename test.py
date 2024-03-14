c2b8a7383d04c1a7d09b893fcf75b96604acc500
Doo-mon <zhanzhh6@mail2.sysu.edu.cn>
2024-03-14 14:29:27 +0800

    small update

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-# ------------------------------------------------------------------------
-# Copyright (c) 2022 megvii-model. All Rights Reserved.
-# ------------------------------------------------------------------------
-# Modified from BasicSR (https://github.com/xinntao/BasicSR)
-# Copyright 2018-2020 BasicSR Authors
-# ------------------------------------------------------------------------
 import logging
 import torch
 from os import path as osp
@@ -17,22 +11,22 @@ from basicsr.train import parse_options
 
 def main():
     # parse options, set distributed setting, set ramdom seed
-    opt = parse_options(is_train=False)
+    opt = parse_options(is_train=False) # 注意这个函数是从basicsr里面的train.py里面导入的
 
+    # 设置为True时 cuDNN自动调优器会在网络的启动阶段尝试运行不同的卷积算法，并选择最快的一个
     torch.backends.cudnn.benchmark = True
+    # 在科研实验中对结果进行精确复现或者当你需要确保每次运行得到的结果完全一致时设置
     # torch.backends.cudnn.deterministic = True
 
     # mkdir and initialize loggers
     make_exp_dirs(opt)
-    log_file = osp.join(opt['path']['log'],
-                        f"test_{opt['name']}_{get_time_str()}.log")
-    logger = get_root_logger(
-        logger_name='basicsr', log_level=logging.INFO, log_file=log_file)
+    log_file = osp.join(opt['path']['log'], f"test_{opt['name']}_{get_time_str()}.log")
+    logger = get_root_logger(logger_name='basicsr', log_level=logging.INFO, log_file=log_file)
     logger.info(get_env_info())
     logger.info(dict2str(opt))
 
     # create test dataset and dataloader
-    test_loaders = []
+    test_loaders = [] # 用列表来存放不同的测试集
     for phase, dataset_opt in sorted(opt['datasets'].items()):
         if 'test' in phase:
             dataset_opt['phase'] = 'test'
@@ -44,11 +38,10 @@ def main():
             dist=opt['dist'],
             sampler=None,
             seed=opt['manual_seed'])
-        logger.info(
-            f"Number of test images in {dataset_opt['name']}: {len(test_set)}")
+        logger.info(f"Number of test images in {dataset_opt['name']}: {len(test_set)}")
         test_loaders.append(test_loader)
 
-    # create model
+    # 权重会在初始化的时候就加载
     model = create_model(opt)
 
     for test_loader in test_loaders:
